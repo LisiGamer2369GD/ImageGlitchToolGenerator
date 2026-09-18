2e85cb5dc2e96569095721a15b50c8257e99969b
Lisi <lisigamerd@gmail.com>
2026-09-18 14:57:56 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import io
+import random
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
+import cv2
+import numpy as np
+from PIL import Image, ImageTk
+
+# no minty, loloolololol
